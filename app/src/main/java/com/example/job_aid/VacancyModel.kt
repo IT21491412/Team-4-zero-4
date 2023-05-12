144ce702de6bab2c5cb0a4ab65955e8eecfa1893
IT21572388~ <shachiniyureshika@gmail.com>
2023-05-12 20:31:00 +0530

    Vacancy done done

---
+++ app/src/main/java/com/example/job_aid/VacancyModel.kt
@@ -0,0 +1,8 @@
+package com.example.job_aid
+
+data class VacancyModel(
+        var vid: String? = null,
+        var jbRl: String? = null,
+        var jobDes: String? = null,
+        var comOver: String?=null
+    )
